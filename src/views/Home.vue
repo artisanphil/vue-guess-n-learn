718dca367ea088c2da25aad6a26163acfc64aeee
Philip <philip@learnclick.com>
2021-05-08 11:14:03 +0100

    added list objects component and added type script http helper and interface for IObjects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ListObjects />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ListObjects from '@/components/ListObjects.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    ListObjects,
   },
 })
 export default class Home extends Vue {}
