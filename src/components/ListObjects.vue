92b8ef4a9a1866d67e4793587f2f30f52eb64a97
Philip <philip@learnclick.com>
2021-05-09 17:54:45 +0100

    added tailwind

--- src/components/ListObjects.vue
+++ src/components/ListObjects.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="ListObjects">
-    <h1>Please choose a character for the computer to guess</h1>
-    <div class=objects :key="index" v-for="(object, index) in objects">
-      <div class=object>
+    <h1 class="text-center text-xl mt-4">Please choose a character for the computer to guess</h1>
+    <main class="container px-8 pt-24 mx-auto lg:px-4">
+    <div class="flex flex-wrap">
+      <div class="px-8 py-6 lg:w-1/3 md:w-full" :key="index" v-for="(object, index) in objects">
         {{ object.name }}
        </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
