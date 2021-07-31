8ac78adfb30abe08e71736b7b366a06512f0608a
Philip <philip@learnclick.com>
2021-07-31 16:52:09 +0100

    added guess page, listing attributes

---
+++ src/views/Guess.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="container px-8 pt-2 mx-auto lg:px-4">
+    <div class="flex">
+      <AttributeList @messageFromChild="attributeSelected" />
+    </div>
+  </main>
+  <confirm-dialogue ref="confirmDialogue"></confirm-dialogue>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import AttributeList from "../components/AttributeList.vue";
+import ConfirmDialogue from "../components/ConfirmDialogue.vue";
+
+@Options({
+  components: {
+    AttributeList,
+    ConfirmDialogue,
+  },
+})
+export default class Guess extends Vue {
+
+  $refs!: {
+    confirmDialogue: InstanceType<typeof ConfirmDialogue>;
+  };
+}
+</script>
