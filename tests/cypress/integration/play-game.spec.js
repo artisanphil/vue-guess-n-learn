8f1ece3060b0111c41bd6fee02c0aae954adcf63
Philip <philip@learnclick.com>
2021-08-28 19:47:18 +0100

    got test working and extracted steps into separate methods

--- tests/cypress/integration/play-game.spec.js
+++ tests/cypress/integration/play-game.spec.js
@@ -2,34 +2,58 @@
 describe('Play Game', () => {
   it('Returns expected elements', () => {
 
-    cy.visit('/')
+    cy.visit('')
 
-    cy.get('body').should('include.text', 'Please choose a character for the computer to guess');
+    selectCharacter();
 
-    cy.contains('Liam').click();
+    computerGuess();
 
-    cy.get('.swal2-popup').should('include.text', 'You have selected Liam');
+    selectAttribute();
 
-    cy.intercept('GET', '/api/computer-guess', { fixture: 'computer-guess1.json' })
+    askQuestion();
 
-    cy.get('.swal2-confirm').contains('OK').click();
+    cy.get('body').should('include.text', 'Continue');
+  })
+})
 
-    cy.get('.swal2-popup').should('include.text', 'Computer Guess');
-    cy.get('.swal2-popup').should('include.text', 'Does the person wear glasses?');
+function selectCharacter()
+{
+  cy.get('body').should('include.text', 'Please choose a character for the computer to guess');
 
-    cy.get('.swal2-confirm').contains('Yes').click();
+  cy.contains('Liam').click();
 
-    cy.get('#attributes').contains('brown eyes').click();
+  cy.get('.swal2-popup').should('include.text', 'You have selected Liam');
+}
 
-    cy.intercept('GET', '/api/user-guess?choice=brown eyes', { fixture: 'mchoice-brown-eyes.json' })
+function computerGuess()
+{
+  cy.intercept('GET', '/api/computer-guess', { fixture: 'computer-guess1.json' })
 
-    cy.get('.mchoice-item').eq(0).click();
+  cy.get('.swal2-confirm').contains('OK').click();
 
-    cy.get('.mchoice-item').should('have.class', 'border-red-400').eq(0);
+  cy.get('.swal2-popup').should('include.text', 'Computer Guess');
+  cy.get('.swal2-popup').should('include.text', 'Does the person wear glasses?');
 
-    cy.get('.mchoice-item').eq(1).click();
+  cy.get('.swal2-confirm').contains('Yes').click();
+}
+
+function selectAttribute()
+{
+  cy.get('#attributes').contains('brown eyes').click();
+}
+
+function askQuestion()
+{
+    cy.request('POST', 'http://localhost:8000/api/computer-select', { selection: 'Tim' })
+
+    cy.get('.mchoice-item').not(':contains("brown eyes")').eq(0).click();
+    cy.get('.mchoice-item').not(':contains("brown eyes")').eq(0).should('have.class', 'border-red-400');
+
+    cy.get('.mchoice-item').contains('brown eyes').click();
 
     cy.get('.swal2-popup').should('include.text', 'You asked');
     cy.get('.swal2-popup').should('include.text', 'Does the person have brown eyes?');
-  })
-})
+    cy.get('.swal2-popup').should('include.text', 'Answer: Yes');
+
+    cy.get('.swal2-confirm').contains('OK').click();
+}
