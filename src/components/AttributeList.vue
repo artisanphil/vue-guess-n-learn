52d9e656bfef654ce776fa6afe7c1706a1b7dd76
Philip <philip@learnclick.com>
2021-09-22 19:47:12 +0100

    added attribute image and made attributes page responsive

--- src/components/AttributeList.vue
+++ src/components/AttributeList.vue
@@ -1,30 +1,83 @@
 <template>
-<div id=attributes class="flex flex-wrap w-full">
-        <div
-          class="px-8 py-6 lg:w-1/4 w-1/3 border hover:shadow-lg cursor-pointer"
-          :key="index"
-          v-for="(attribute, index) in attributes"
-          @click="select(index)"
-        >
-          {{ attribute }}
-        </div>
+  <div id="container" class="grid w-full">
+    <div
+      id="attributes"
+      class="border hover:shadow-lg cursor-pointer"
+      style="width: 100%; vertical-align: center"
+      :key="index"
+      v-for="(attribute, index) in attributes"
+      @click="select(index)"
+    >
+      <div class="attributeText">
+        {{ attribute.name }}
       </div>
+      <img class="attributeImage inline" v-bind:src="attribute.image" />
+    </div>
+  </div>
 </template>
 
+<style>
+@media (orientation: landscape) {
+  #container {
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+  }
+  #attributes {
+    height: 19vh;
+  }
+  .attributeText {
+    margin-top: 0.5vh;
+    margin-bottom: 0.5vh;
+    font-size: 3vh;
+  }
+  .attributeImage {
+    height: 13vh;
+  }
+}
+
+@media (orientation: portrait) {
+  #container {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+  #attributes {
+    height: 14vh;
+  }
+  .attributeText {
+    margin-top: 0.5vh;
+    margin-bottom: 0.5vh;
+    font-size: 2vh;
+  }
+  .attributeImage {
+    height: 9vh;
+  }
+}
+</style>
+
 <script lang="ts">
-import { Vue } from 'vue-class-component';
+import { Vue } from "vue-class-component";
+import { IAttribute } from "../interfaces/IAttribute";
 import { get } from "../helpers/http";
 
 export default class AttributeList extends Vue {
-
-  protected attributes: Array<string> = [];
+  protected attributes: Array<IAttribute> = [];
 
   async created(): Promise<void> {
-    this.attributes = await get<Array<string>>("/api/remaining-attributes");
+    let attributes = await get<Array<string>>("/api/remaining-attributes");
+    const imagePath =
+      process.env.VUE_APP_BACKEND + "/images/character-attributes/";
+    let allAttributes: IAttribute[] = [];
+
+    for (var i = 0; i < attributes.length; i++) {
+      //let image = imagePath + attributes[i].replace(" ", "-") + ".png";
+      let image = "http://via.placeholder.com/150x100";
+      let attribute = { name: attributes[i], image: image };
+      allAttributes[i] = attribute;
+    }
+
+    this.attributes = allAttributes;
   }
 
   select(index: number): void {
-    this.$emit('messageFromChild', this.attributes, index);
+    this.$emit("messageFromChild", this.attributes, index);
   }
 }
 </script>
--- src/views/PickAttribute.vue
+++ src/views/PickAttribute.vue
@@ -1,8 +1,16 @@
 <template>
-  <main class="container px-8 pt-2 lg:px-4">
+  <main class="px-8 pt-2 lg:px-4">
     <div class="flex">
-      <AttributeList v-show="displayAttributes" @messageFromChild="attributeSelected" />
-      <MultipleChoice v-show="displayMChoice" :questions="questions" :attribute="attribute" @messageFromChild="questionSelected" />
+      <AttributeList
+        v-show="displayAttributes"
+        @messageFromChild="attributeSelected"
+      />
+      <MultipleChoice
+        v-show="displayMChoice"
+        :questions="questions"
+        :attribute="attribute"
+        @messageFromChild="questionSelected"
+      />
     </div>
   </main>
 </template>
@@ -12,56 +20,59 @@ import { defineComponent } from "vue";
 import AttributeList from "../components/AttributeList.vue";
 import MultipleChoice from "../components/MultipleChoice.vue";
 import { post } from "../helpers/http";
-import Swal from 'sweetalert2'
-import router from '../router';
+import Swal from "sweetalert2";
+import router from "../router";
 import { IObject } from "../interfaces/IObject";
 
 export default defineComponent({
-	name: "PickAttribute",
-	data() {
-		return {
-			displayAttributes: true,
+  name: "PickAttribute",
+  data() {
+    return {
+      displayAttributes: true,
       displayMChoice: false,
       questions: [],
-      attribute: ''
-		};
-	},
+      attribute: "",
+    };
+  },
   components: {
     AttributeList,
-    MultipleChoice
+    MultipleChoice,
   },
-	methods: {
-      async attributeSelected(attributes: Array<string>, index: number): Promise<void> {
-        this.attribute = attributes[index];
-        this.displayAttributes = false;
+  methods: {
+    async attributeSelected(
+      attributes: Array<string>,
+      index: number
+    ): Promise<void> {
+      this.attribute = attributes[index];
+      this.displayAttributes = false;
 
-        let url = `/api/user-guess?choice=${this.attribute}&questiontype=multiple-choice`;
-        let response = await fetch(process.env.VUE_APP_BACKEND + url);
-        let questionType = await response.headers.get('Question-Type');
-        this.questions = await response.json();
+      let url = `/api/user-guess?choice=${this.attribute}&questiontype=multiple-choice`;
+      let response = await fetch(process.env.VUE_APP_BACKEND + url);
+      let questionType = await response.headers.get("Question-Type");
+      this.questions = await response.json();
 
-        switch(questionType) {
-          case 'multiple-choice':
-            this.displayMChoice = true;
-            break;
-        }
+      switch (questionType) {
+        case "multiple-choice":
+          this.displayMChoice = true;
+          break;
+      }
     },
     async questionSelected(data: any): Promise<void> {
       let response = await post<any>("/api/user-guess", data);
-      let answer = response.correct ? 'Yes' : 'No';
+      let answer = response.correct ? "Yes" : "No";
 
       Swal.fire({
-        title: 'You asked:',
-        html: data.sentence + '<br><br><b>Answer</b>: ' + answer,
-        confirmButtonText: 'OK'
-      })
+        title: "You asked:",
+        html: data.sentence + "<br><br><b>Answer</b>: " + answer,
+        confirmButtonText: "OK",
+      });
 
-      let matchingNames = response.matching.map(function(object: IObject) {
+      let matchingNames = response.matching.map(function (object: IObject) {
         return object.name;
       });
       let matchingJson = JSON.stringify(matchingNames);
-      router.push({name: 'Home', params: { matching: matchingJson }});
-    }
-	},
+      router.push({ name: "Home", params: { matching: matchingJson } });
+    },
+  },
 });
 </script>
