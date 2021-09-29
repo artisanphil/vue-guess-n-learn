e15810b1a1f7fa53de45dd27758baecbd9a540c7
Philip <philip@learnclick.com>
2021-09-29 18:58:01 +0100

    moved selected character and continue button into separate component, not displaying on page load

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,61 +1,48 @@
 <template>
   <main class="container px-8 pt-2 mx-auto lg:px-4">
-    <div class="flex-auto sm:hidden  md:hidden lg:hidden mx-auto">
-      <h3 class="text-lg">Your character</h3>
-      <div id="your-selection" class="w-20 mx-auto">
-        <img v-bind:src="yourSelection" />
-      </div>
-      <div id="ask" class="my-2">
-        <button @click="guess()" class="swal2-confirm swal2-styled">Continue</button>
-      </div>
-    </div>
     <h1 class="text-center text-2xl mt-4 pb-4">
       Please choose a character for the computer to guess
     </h1>
     <div class="flex">
       <ObjectList @messageFromChild="objectSelected" />
-      <div class="flex-auto hidden sm:block md:2/6">
-        <h3 class="text-lg">Your character</h3>
-        <div id="your-selection">
-          <img v-bind:src="yourSelection" />
-        </div>
-        <div id="ask" class="my-2">
-          <button @click="guess()" class="swal2-confirm swal2-styled">Continue</button>
-        </div>
-      </div>
+      <SelectedCharacter v-if="characterSelected" :yourSelection="yourSelection" />
     </div>
-    
   </main>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import ObjectList from "../components/ObjectList.vue";
-import Swal from 'sweetalert2'
+import SelectedCharacter from "../components/SelectedCharacter.vue";
+import Swal from "sweetalert2";
 import { IObject } from "../interfaces/IObject";
 import GuessClass from "../classes/Guess";
 import ObjectClass from "../classes/ObjectClass";
 import { get, post } from "../helpers/http";
-import router from '../router';
+import router from "../router";
 
 @Options({
   data: () => {
     return {
       yourSelection: "",
+      characterSelected: false,
     };
   },
   components: {
     ObjectList,
+    SelectedCharacter,
   },
 })
 export default class Home extends Vue {
-  protected yourSelection: string = '' as string;
+  protected yourSelection: string = "" as string;
+  protected characterSelected: boolean = false as boolean;
 
-  async created(): Promise<void>  {
-    if(Object.keys(this.$route.params).length > 0) {
+  async created(): Promise<void> {
+    if (Object.keys(this.$route.params).length > 0) {
       let objectSelected = await get<IObject>("/api/select");
 
       this.yourSelection = ObjectClass.getImage(objectSelected);
+      this.characterSelected = true;
     }
   }
 
@@ -66,39 +53,42 @@ export default class Home extends Vue {
     const ok = await Swal.fire({
       title: "Your selection",
       text: "You have selected " + name,
-      reverseButtons: true
+      reverseButtons: true,
     });
 
     if (ok) {
       await post<any>("/api/select", data);
       this.yourSelection = ObjectClass.getImage(objects[index]);
+      this.characterSelected = true;
       this.guess();
     }
   }
 
   async guess(sentence?: string, computerChoice?: string): Promise<void> {
-    let question = await GuessClass.getComputerQuestion(sentence, computerChoice);
+    let question = await GuessClass.getComputerQuestion(
+      sentence,
+      computerChoice
+    );
     let answer = await GuessClass.displayGuessDialog(question);
 
-    if(question.choice == '') {
-      router.push('game-over');
+    if (question.choice == "") {
+      router.push("game-over");
       return;
     }
 
     const data = { correct: answer, choice: question.choice };
     let response = await post<any>("/api/computer-guess", data);
 
-    if(answer !== response.correct) {
+    if (answer !== response.correct) {
       return this.guess(question.sentence, question.choice);
     }
 
-    router.push('pick-attribute');
+    router.push("pick-attribute");
   }
 }
 </script>
 
 <style>
-  @media (min-width: 668px) {
-
-  }
+@media (min-width: 668px) {
+}
 </style>
