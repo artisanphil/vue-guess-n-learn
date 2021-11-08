30a84cf01798b4afeaddf1d46eebdb9def8a2cf2
Philip <philip@learnclick.com>
2021-11-08 18:54:28 +0000

    returns message win or lost after user selects character

--- src/views/GameOver.vue
+++ src/views/GameOver.vue
@@ -1,17 +1,25 @@
 <template>
-  <h1 class="text-center text-xl my-10">
+  <h1 class="text-center text-2xl mt-10 mb-2">
     Game Over!
   </h1>
-  <button @click="playAgain()" class="swal2-confirm swal2-styled">Play again</button>
+  <h2 class="text-xl" v-if="win">You win!</h2>
+  <h2 v-else class="text-xl">You lost &#x1F61E;</h2>
+  <button @click="playAgain()" class="swal2-confirm swal2-styled mt-10">Play again</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import router from '../router';
 
-
 export default defineComponent({
+  data() {
+      return {
+        win: this.$route.params.win,
+      }
+  },
 	name: "GameOver",
+  created: void {
+  },
 	methods: {
       playAgain(): void {
         router.push('/');
