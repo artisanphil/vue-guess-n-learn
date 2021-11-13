d020f60d7001f87005a3d47195728fc2253f5d07
Philip <philip@learnclick.com>
2021-11-13 17:59:46 +0000

    added character to dialog

--- src/classes/Guess.ts
+++ src/classes/Guess.ts
@@ -1,6 +1,8 @@
 import { get } from "../helpers/http";
 import { IComputerGuess } from "../interfaces/IComputerGuess";
 import Swal from 'sweetalert2'
+import { IObject } from "../interfaces/IObject";
+import ObjectClass from "../classes/ObjectClass";
 
 class Guess {
     async getComputerQuestion(sentence?: string, computerChoice?: string): Promise<IComputerGuess> {
@@ -32,9 +34,12 @@ class Guess {
         });
       }
 
+      const objectSelected = await get<IObject>("/api/select");
+      const image = ObjectClass.getImage(objectSelected)
+
       const dialog = await dialogButtons.fire({
         title: 'Computer Guess',
-        html: question.sentence,
+        html: '<img width=50% src="' + image + '" style="margin:auto;"><br/ >' + question.sentence,
         reverseButtons: true
       });
 
